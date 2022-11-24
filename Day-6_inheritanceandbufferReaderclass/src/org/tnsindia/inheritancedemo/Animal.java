ef97dae3819ad268275091c4e62b9b8cb74f2180
Shruti Sonawane <Shruti Sonawane@LAPTOP-ODEI6MAM>
2022-11-24 23:13:43 +0530

    commit

---
+++ Day-6_inheritanceandbufferReaderclass/src/org/tnsindia/inheritancedemo/Animal.java
@@ -0,0 +1,11 @@
+package org.tnsindia.inheritancedemo;
+//parent class
+//Demo on Single Inheritance
+public class Animal {
+	protected String name;
+	public void display ()
+	{
+			System.out.println("The name of the animal is: "+name);
+	}
+
+}
