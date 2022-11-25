2b30ddf792e4bd7913742985cf7cd6af27d00a17
Shruti Sonawane <Shruti Sonawane@LAPTOP-ODEI6MAM>
2022-11-25 11:08:13 +0530

    commit

---
+++ Day-1EclipseandGithub/src/org/tnsIndia/gitdemo/DemoForGithubConnection.java
@@ -0,0 +1,12 @@
+package org.tnsIndia.gitdemo;
+
+public class DemoForGithubConnection {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to TNS India Foundation");
+		
+		
+	}
+
+}
