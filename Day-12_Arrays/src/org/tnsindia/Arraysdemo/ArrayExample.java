f1ad4e98d190fa981c14caedbadf126acf8dedd2
Shruti Sonawane <Shruti Sonawane@LAPTOP-ODEI6MAM>
2022-12-08 04:59:26 +0530

    commit

---
+++ Day-12_Arrays/src/org/tnsindia/Arraysdemo/ArrayExample.java
@@ -0,0 +1,16 @@
+package org.tnsindia.Arraysdemo;
+
+	//demo on to know the class of array
+	public class ArrayExample {
+
+		public static void main(String[] args) {
+			//String[] arr= {"January","February","March","April","May"};//String
+			//int arr[]= {12,4,6,9};//I
+			float arr[]= {12.6f,4.9f,69.4f};//F
+			/*getClass() Returns the runtime class of this Object*/
+			/*getName() Returns the canonical name of the underlying class asdefined by The Java Language Specification.*/
+			System.out.println(arr.getClass().getName());
+
+		}
+
+	}
