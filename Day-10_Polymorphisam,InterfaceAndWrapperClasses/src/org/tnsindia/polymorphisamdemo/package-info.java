0cf71a3f4e9b171aa0c5c1e3d280aed4cf492b2a
Shruti Sonawane <Shruti Sonawane@LAPTOP-ODEI6MAM>
2022-12-17 20:55:12 +0530

    commit

--- Day-10_Polymorphisam,InterfaceAndWrapperClasses/src/org/tnsindia/polymorphisamdemo/package-info.java
+++ Day-10_Polymorphisam,InterfaceAndWrapperClasses/src/org/tnsindia/polymorphisamdemo/package-info.java
@@ -1,1 +1,20 @@
-package org.tnsindia.polymorphisamdemo;
+class Test
+{
+	static int a;
+	static
+	{
+		a=4;
+		System.out.println("insude static blockn");
+		System.out.println("a="+a);
+	}
+	Test()
+	{
+		System.out.println("ninside constructorn");
+		a=10;
+	}
+	public static void main(string[] args)
+	{
+		Test obj=new Test();
+		obj.func();
+	}
+}
